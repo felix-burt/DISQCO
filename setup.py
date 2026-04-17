4ddc5e1b286a5ecbf527482a92a5d29c28677402
Elie <eliebenshlomo@outlook.com>
2026-04-17 11:23:37 +1000

    commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bosonic-disqco",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["numpy>=1.26,<2.3.4",
